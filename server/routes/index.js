197a0128aca1b1611fb129df366404deba82a5a9
gaurav sharma <sharma02gaurav@gmail.com>
2021-02-11 13:31:56 +0530

    chore(logs): add utility log

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,6 +6,7 @@
 export default (app) => {
 	app.get('/baremetal', (req, res) => res.send('Hello Bare Metal Express Server'));
 	app.post('/pr', (req, res) => {
+		console.log('PR webhook invoked');
 		console.log(req.body);
 		res.send({ code: 100 });
 	});
